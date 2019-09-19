7970533868e7d1a5fdc8cd5ecfd389a02420cf8d
geraldolucaspoli <locaspolix3@gmail.com>
2019-09-19 13:17:23 -0300

    Script criado em PowerShell

--- install-zabbix-agente-3.4.6-windows-x64.ps1
+++ install-zabbix-agente-3.4.6-windows-x64.ps1
@@ -1,35 +1,30 @@
 @echo off
-REM #
-REM #versao mark-v0.2#
-REM #
-REM #NOME#
-REM #instala_zabbix.exe#
-REM #
+REM #versao mark-v0.3#
 REM #DESCRICAO#
 REM #Instalao Zabbix via rede nos servidores Windows#
-REM #
-REM #
-REM #GERALDO LUCAS POLI (27/07/2018)#
-REM # Verifica existencia diretório Zabbix #
+REM #GERALDO LUCAS POLI (19/09/2018)#
+REM # Verifica existencia diret�rio Zabbix #
 
 REM #MODULO 1#
 IF EXIST "c:\Zabbix\zabbix_agentd.exe" (exit) ELSE (echo "Iniciando a instalacao do Zabbix Agent")
-REM #
 REM # Exibe mensagem na tela#
 echo Copiando Arquivos
 
-
-REM #MODULO 2 - Criar o diretório na máquina#
+REM #MODULO 2 - CRIAR DIRETORIO NA MAQUINA#
 mkdir c:\zabbix
 echo Pasta zabbix criada no C:
 
-
-REM #MODULO 3 - Realiza a cópia dos binários para o sistema#
+REM #MODULO 3 - Realiza a c�pia dos bin�rios para o sistema#
 xcopy \\192.168.2.12\zabbix\zabbix_agents_3.4.6.win\bin\win64\zabbix_agentd.exe c:\zabbix\
 xcopy \\192.168.2.12\zabbix\zabbix_agents_3.4.6.win\bin\win64\zabbix_get.exe c:\zabbix\
 xcopy \\192.168.2.12\zabbix\zabbix_agents_3.4.6.win\bin\win64\zabbix_sender.exe c:\zabbix\
 
-REM #MODULO 4 *PUNK* - Cria arquivo de configuração e define nome do servidor #
+cd c:\zabbix
+Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/geraldolucaspoli/install-zabbix-agente-windows/master/zabbix_agents_3.4.6.win/bin/win64/zabbix_agentd.exe -OutFile zabbix_agentd.exe
+
+
+
+REM #MODULO 4 *PUNK* - Cria arquivo de configura��o e define nome do servidor #
 echo Criando Arquivos de configuracao
 echo Server=192.168.1.26 > c:\zabbix\zabbix_agentd.conf
 echo Hostname=%COMPUTERNAME% >> c:\zabbix\zabbix_agentd.conf
@@ -40,7 +35,7 @@ echo Timeout=5 >> c:\zabbix\zabbix_agentd.conf
 echo EnableRemoteCommands=1 >> C:\zabbix\zabbix_agentd.conf
 
 REM #MODULO 4 - Ajustes Finais #
-echo Instalando o Serviço
+echo Instalando o Servi�o
 C:\zabbix\zabbix_agentd.exe -i -c C:\zabbix\zabbix_agentd.conf
 C:\zabbix\zabbix_agentd.exe -s -c C:\zabbix\zabbix_agentd.conf
 echo As configuracoes para o servidor %COMPUTERNAME% foram criadas em %date% as %time%
